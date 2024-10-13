0a110c7f1e451b1cb24b4fc3f630722e73da423b
liangquan <liangquan0000@outlook.com>
2024-10-13 20:49:58 +0800

    更改parcel版本

--- src/main/main.js
+++ src/main/main.js
@@ -1,3 +1,41 @@
 import * as THREE from 'three';
 
-console.log(THREE);
+// console.log(THREE);
+
+// 了解three.js的基本使用
+
+// 创建场景
+const scene = new THREE.Scene();
+
+// 创建相机-透视相机
+// 参数1：视场角-75度，2：长宽比-屏幕宽高比，3：近截面，4：远截面
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+// 设置相机位置
+camera.position.set(0, 0, 10);
+scene.add(camera);
+
+// 添加物体
+// 创建几何体
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+// 创建材质 - 使用 MeshBasicMaterial
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+// 根据几何体和材质创建物体
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+// 将几何体添加到场景中
+scene.add(cube);
+
+// 初始化渲染器 
+const renderer = new THREE.WebGLRenderer();
+// 设置渲染的尺寸大小
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+// 将webgl渲染的canvas内容添加到body
+document.addEventListener('DOMContentLoaded', function () {
+    document.body.appendChild(renderer.domElement);
+    // 使用渲染器，通过相机将场景渲染进来
+    renderer.render(scene, camera);
+});
+
+
+
